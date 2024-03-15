80c45c3c339854e58ad9a3c395f9b3ea5cce9c8e
thunderhoser <lagerqui@ualberta.ca>
2024-03-15 15:28:59 -0600

    FOO

--- ml_for_national_blend/make_templates_exp01.py
+++ ml_for_national_blend/make_templates_exp01.py
@@ -106,9 +106,9 @@ METRIC_FUNCTION_STRINGS = [
 NUM_CONV_LAYERS_PER_BLOCK = 1
 NUM_NWP_LEAD_TIMES = 2
 
-OPTIMIZER_FUNCTION = keras.optimizers.Nadam(gradient_accumulation_steps=6)
+OPTIMIZER_FUNCTION = keras.optimizers.Nadam(gradient_accumulation_steps=24)
 OPTIMIZER_FUNCTION_STRING = (
-    'keras.optimizers.Nadam(gradient_accumulation_steps=6)'
+    'keras.optimizers.Nadam(gradient_accumulation_steps=24)'
 )
 
 DEFAULT_OPTION_DICT = {
