919b42a652d614c9be21d70ee796f771351d9f00
thunderhoser <lagerqui@ualberta.ca>
2024-05-14 15:03:16 -0600

    Added evaluation-plotting code.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 PACKAGE_NAMES = [
     'ml_for_national_blend', 'ml_for_national_blend.io',
     'ml_for_national_blend.utils', 'ml_for_national_blend.machine_learning',
-    'ml_for_national_blend.plotting', 'ml_for_national_blend.scripts'
+    'ml_for_national_blend.plotting', 'ml_for_national_blend.scripts',
+    'ml_for_national_blend.outside_code'
 ]
 KEYWORDS = [
     'machine learning', 'deep learning', 'artificial intelligence',
