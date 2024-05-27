a5956a9f372ae62f92aab6c86c739716d39867cc
thunderhoser <lagerqui@ualberta.ca>
2024-05-27 16:54:32 -0600

    FOO

---
+++ ml_for_national_blend/nwp_plotting.py
@@ -0,0 +1,194 @@
+"""Methods for plotting NWP output."""
+
+import os
+import sys
+import numpy
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import temperature_conversions as temperature_conv
+import nwp_model_utils
+import target_plotting
+
+TOLERANCE = 1e-6
+NAN_COLOUR = numpy.full(3, 152. / 255)
+
+PASCALS_TO_MB = 0.01
+FRACTION_TO_PERCENT = 100.
+METRES_TO_MM = 1000.
+METRES_TO_DEKAMETRES = 0.1
+
+FIELD_NAME_TO_FANCY = {
+    nwp_model_utils.MSL_PRESSURE_NAME: 'MSL pressure (hPa)',
+    nwp_model_utils.SURFACE_PRESSURE_NAME: 'surface pressure (hPa)',
+    nwp_model_utils.TEMPERATURE_2METRE_NAME: r'2-m temperature ($^{\circ}$C)',
+    nwp_model_utils.DEWPOINT_2METRE_NAME: r'2-m dewpoint ($^{\circ}$C)',
+    nwp_model_utils.RELATIVE_HUMIDITY_2METRE_NAME: r'2-m relative humidity (%)',
+    nwp_model_utils.U_WIND_10METRE_NAME: r'10-m zonal wind (m s$^{-1}$)',
+    nwp_model_utils.V_WIND_10METRE_NAME: r'10-m meridional wind (m s$^{-1}$)',
+    nwp_model_utils.WIND_GUST_10METRE_NAME: r'10-m wind gust (m s$^{-1}$)',
+    nwp_model_utils.PRECIP_NAME: 'accumulated precip (mm)',
+    nwp_model_utils.HEIGHT_500MB_NAME: '500-hPa height (dam)',
+    nwp_model_utils.HEIGHT_700MB_NAME: '700-hPa height (dam)',
+    nwp_model_utils.RELATIVE_HUMIDITY_500MB_NAME:
+        '500-hPa relative humidity (%)',
+    nwp_model_utils.RELATIVE_HUMIDITY_700MB_NAME:
+        '700-hPa relative humidity (%)',
+    nwp_model_utils.RELATIVE_HUMIDITY_850MB_NAME:
+        '850-hPa relative humidity (%)',
+    nwp_model_utils.U_WIND_500MB_NAME: r'500-hPa zonal wind (m s$^{-1}$)',
+    nwp_model_utils.U_WIND_700MB_NAME: r'700-hPa zonal wind (m s$^{-1}$)',
+    nwp_model_utils.U_WIND_1000MB_NAME: r'1000-hPa zonal wind (m s$^{-1}$)',
+    nwp_model_utils.V_WIND_500MB_NAME: r'500-hPa meridional wind (m s$^{-1}$)',
+    nwp_model_utils.V_WIND_700MB_NAME: r'700-hPa meridional wind (m s$^{-1}$)',
+    nwp_model_utils.V_WIND_1000MB_NAME:
+        r'1000-hPa meridional wind (m s$^{-1}$)',
+    nwp_model_utils.TEMPERATURE_850MB_NAME:
+        r'850-hPa temperature ($^{\circ}$C)',
+    nwp_model_utils.TEMPERATURE_950MB_NAME:
+        r'950-hPa temperature ($^{\circ}$C)',
+    nwp_model_utils.MIN_RELATIVE_HUMIDITY_2METRE_NAME: 'minimum 2-m RH (%)',
+    nwp_model_utils.MAX_RELATIVE_HUMIDITY_2METRE_NAME: 'maximum 2-m RH (%)'
+}
+
+FIELD_NAME_TO_CONV_FACTOR = {
+    nwp_model_utils.MSL_PRESSURE_NAME: PASCALS_TO_MB,
+    nwp_model_utils.SURFACE_PRESSURE_NAME: PASCALS_TO_MB,
+    nwp_model_utils.TEMPERATURE_2METRE_NAME:
+        temperature_conv.kelvins_to_celsius,
+    nwp_model_utils.DEWPOINT_2METRE_NAME: temperature_conv.kelvins_to_celsius,
+    nwp_model_utils.RELATIVE_HUMIDITY_2METRE_NAME: FRACTION_TO_PERCENT,
+    nwp_model_utils.U_WIND_10METRE_NAME: 1.,
+    nwp_model_utils.V_WIND_10METRE_NAME: 1.,
+    nwp_model_utils.WIND_GUST_10METRE_NAME: 1.,
+    nwp_model_utils.PRECIP_NAME: METRES_TO_MM,
+    nwp_model_utils.HEIGHT_500MB_NAME: METRES_TO_DEKAMETRES,
+    nwp_model_utils.HEIGHT_700MB_NAME: METRES_TO_DEKAMETRES,
+    nwp_model_utils.RELATIVE_HUMIDITY_500MB_NAME: FRACTION_TO_PERCENT,
+    nwp_model_utils.RELATIVE_HUMIDITY_700MB_NAME: FRACTION_TO_PERCENT,
+    nwp_model_utils.RELATIVE_HUMIDITY_850MB_NAME: FRACTION_TO_PERCENT,
+    nwp_model_utils.U_WIND_500MB_NAME: 1.,
+    nwp_model_utils.U_WIND_700MB_NAME: 1.,
+    nwp_model_utils.U_WIND_1000MB_NAME: 1.,
+    nwp_model_utils.V_WIND_500MB_NAME: 1.,
+    nwp_model_utils.V_WIND_700MB_NAME: 1.,
+    nwp_model_utils.V_WIND_1000MB_NAME: 1.,
+    nwp_model_utils.TEMPERATURE_850MB_NAME: temperature_conv.kelvins_to_celsius,
+    nwp_model_utils.TEMPERATURE_950MB_NAME: temperature_conv.kelvins_to_celsius,
+    nwp_model_utils.MIN_RELATIVE_HUMIDITY_2METRE_NAME: FRACTION_TO_PERCENT,
+    nwp_model_utils.MAX_RELATIVE_HUMIDITY_2METRE_NAME: FRACTION_TO_PERCENT
+}
+
+MODEL_NAME_TO_FANCY = {
+    nwp_model_utils.WRF_ARW_MODEL_NAME: 'WRF-ARW',
+    nwp_model_utils.NAM_MODEL_NAME: 'NAM',
+    nwp_model_utils.NAM_NEST_MODEL_NAME: 'NAM Nest',
+    nwp_model_utils.RAP_MODEL_NAME: 'RAP',
+    nwp_model_utils.GFS_MODEL_NAME: 'GFS',
+    nwp_model_utils.HRRR_MODEL_NAME: 'HRRR',
+    nwp_model_utils.GEFS_MODEL_NAME: 'GEFS',
+    nwp_model_utils.GRIDDED_LAMP_MODEL_NAME: 'GLAMP',
+    nwp_model_utils.ECMWF_MODEL_NAME: 'ECMWF',
+    nwp_model_utils.GRIDDED_MOS_MODEL_NAME: 'GMOS'
+}
+
+
+def field_to_colour_scheme(field_name, min_value, max_value):
+    """Returns colour scheme for one target field.
+
+    :param field_name: Field name.  Must be accepted by
+        `urma_utils.check_field_name`.
+    :param min_value: Minimum value in colour scheme.
+    :param max_value: Max value in colour scheme.
+    :return: colour_map_object: Instance of `matplotlib.colors.ListedColormap`.
+    :return: colour_norm_object: Instance of `matplotlib.colors.BoundaryNorm`.
+    """
+
+    nwp_model_utils.check_field_name(field_name)
+    conversion_factor = FIELD_NAME_TO_CONV_FACTOR[field_name]
+
+    if callable(conversion_factor):
+        min_value_converted = conversion_factor(min_value)
+        max_value_converted = conversion_factor(max_value)
+    else:
+        min_value_converted = min_value * conversion_factor
+        max_value_converted = max_value * conversion_factor
+
+    if field_name not in [
+            nwp_model_utils.U_WIND_10METRE_NAME,
+            nwp_model_utils.U_WIND_500MB_NAME,
+            nwp_model_utils.U_WIND_700MB_NAME,
+            nwp_model_utils.U_WIND_1000MB_NAME,
+            nwp_model_utils.V_WIND_10METRE_NAME,
+            nwp_model_utils.V_WIND_500MB_NAME,
+            nwp_model_utils.V_WIND_700MB_NAME,
+            nwp_model_utils.V_WIND_1000MB_NAME
+    ]:
+        colour_map_object = pyplot.get_cmap('viridis')
+        colour_map_object.set_bad(NAN_COLOUR)
+
+        max_value_converted = max([
+            max_value_converted, min_value_converted + TOLERANCE
+        ])
+        colour_norm_object = pyplot.Normalize(
+            vmin=min_value_converted, vmax=max_value_converted
+        )
+
+        return colour_map_object, colour_norm_object
+
+    max_absolute_value = max([
+        numpy.absolute(min_value_converted),
+        numpy.absolute(max_value_converted)
+    ])
+    max_absolute_value = max([max_absolute_value, TOLERANCE])
+
+    colour_map_object = pyplot.get_cmap('seismic')
+    colour_map_object.set_bad(NAN_COLOUR)
+    colour_norm_object = pyplot.Normalize(
+        vmin=-1 * max_absolute_value, vmax=max_absolute_value
+    )
+
+    return colour_map_object, colour_norm_object
+
+
+def plot_field(data_matrix, field_name,
+               latitude_matrix_deg_n, longitude_matrix_deg_e,
+               colour_map_object, colour_norm_object, axes_object,
+               plot_colour_bar):
+    """Plots one field on a lat/long grid.
+
+    This method is a lightweight wrapper for `target_plotting.plot_field`.
+
+    :param data_matrix: See doc for `target_plotting.plot_field`.
+    :param field_name: Field name.  Must be accepted by
+        `urma_utils.check_field_name`.
+    :param latitude_matrix_deg_n: See doc for `target_plotting.plot_field`.
+    :param longitude_matrix_deg_e: Same.
+    :param colour_map_object: Same.
+    :param colour_norm_object: Same.
+    :param axes_object: Same.
+    :param plot_colour_bar: Same.
+    :return: colour_bar_object: Same.
+    """
+
+    nwp_model_utils.check_field_name(field_name)
+    conversion_factor = FIELD_NAME_TO_CONV_FACTOR[field_name]
+
+    return target_plotting.plot_field(
+        data_matrix=(
+            conversion_factor(data_matrix) if callable(conversion_factor)
+            else data_matrix * conversion_factor
+        ),
+        latitude_matrix_deg_n=latitude_matrix_deg_n,
+        longitude_matrix_deg_e=longitude_matrix_deg_e,
+        colour_map_object=colour_map_object,
+        colour_norm_object=colour_norm_object,
+        axes_object=axes_object,
+        plot_colour_bar=plot_colour_bar
+    )
---
+++ ml_for_national_blend/plot_nwp_data.py
@@ -0,0 +1,437 @@
+"""Plots NWP data."""
+
+import os
+import sys
+import argparse
+import numpy
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import time_conversion
+import file_system_utils
+import error_checking
+import border_io
+import nwp_model_io
+import interp_nwp_model_io
+import nwp_model_utils
+import nwp_plotting
+import plotting_utils
+
+HOURS_TO_SECONDS = 3600
+TIME_FORMAT = '%Y-%m-%d-%H'
+
+FIGURE_WIDTH_INCHES = 15
+FIGURE_HEIGHT_INCHES = 15
+FIGURE_RESOLUTION_DPI = 300
+
+INPUT_DIR_ARG_NAME = 'input_nwp_directory_name'
+PLOT_INTERP_DATA_ARG_NAME = 'plot_interp_data'
+MODEL_ARG_NAME = 'model_name'
+INIT_TIME_ARG_NAME = 'init_time_string'
+FORECAST_HOUR_ARG_NAME = 'forecast_hour'
+FIELDS_ARG_NAME = 'field_names'
+MIN_VALUES_ARG_NAME = 'min_colour_values'
+MAX_VALUES_ARG_NAME = 'max_colour_values'
+MIN_PERCENTILES_ARG_NAME = 'min_colour_percentiles'
+MAX_PERCENTILES_ARG_NAME = 'max_colour_percentiles'
+OUTPUT_DIR_ARG_NAME = 'output_dir_name'
+
+INPUT_DIR_HELP_STRING = (
+    'Path to input directory.  The relevant file therein will be found by '
+    '`nwp_model_io.find_file` (if {0:s} == 0) or '
+    '`interp_nwp_model_io.find_file` (if {0:s} == 1).'
+).format(
+    PLOT_INTERP_DATA_ARG_NAME
+)
+PLOT_INTERP_DATA_HELP_STRING = (
+    'Boolean flag.  If 1, will plot data interpolated onto the NBM grid.  '
+    'If 0, will plot data on the native model grid.'
+)
+MODEL_HELP_STRING = (
+    'Model name (must be accepted by `nwp_model_utils.check_model_name`).'
+)
+INIT_TIME_HELP_STRING = 'Forecast-initialization time (format "yyyy-mm-dd-HH").'
+FORECAST_HOUR_HELP_STRING = 'Forecast hour.'
+FIELDS_HELP_STRING = (
+    '1-D list of fields to plot.  Each field must be accepted by '
+    '`nwp_model_utils.check_field_name`.'
+)
+MIN_VALUES_HELP_STRING = (
+    '1-D list of minimum values in colour scheme (must have same length as '
+    '{0:s}).  If you would rather specify minimum values by percentile, leave '
+    'this argument alone.'
+).format(
+    FIELDS_ARG_NAME
+)
+MAX_VALUES_HELP_STRING = 'Same as {0:s} but for max values.'.format(
+    MIN_VALUES_ARG_NAME
+)
+MIN_PERCENTILES_HELP_STRING = (
+    '1-D list of percentiles (must have same length as {0:s}).  For the [k]th '
+    'field, the minimum value in the colour scheme will be percentile {1:s}[k] '
+    'of all {0:s}[k] values.'
+).format(
+    FIELDS_ARG_NAME, MIN_PERCENTILES_ARG_NAME
+)
+MAX_PERCENTILES_HELP_STRING = 'Same as {0:s} but for max values.'.format(
+    MIN_PERCENTILES_ARG_NAME
+)
+OUTPUT_DIR_HELP_STRING = (
+    'Path to output directory.  Figures will be saved here.'
+)
+
+INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + INPUT_DIR_ARG_NAME, type=str, required=True,
+    help=INPUT_DIR_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + PLOT_INTERP_DATA_ARG_NAME, type=int, required=True,
+    help=PLOT_INTERP_DATA_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + MODEL_ARG_NAME, type=str, required=True, help=MODEL_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + INIT_TIME_ARG_NAME, type=str, required=True,
+    help=INIT_TIME_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + FORECAST_HOUR_ARG_NAME, type=int, required=True,
+    help=FORECAST_HOUR_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + FIELDS_ARG_NAME, type=str, nargs='+', required=True,
+    help=FIELDS_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + MIN_VALUES_ARG_NAME, type=float, nargs='+', required=False,
+    default=[1.], help=MIN_VALUES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + MAX_VALUES_ARG_NAME, type=float, nargs='+', required=False,
+    default=[-1.], help=MAX_VALUES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + MIN_PERCENTILES_ARG_NAME, type=float, nargs='+', required=False,
+    default=[1.], help=MIN_PERCENTILES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + MAX_PERCENTILES_ARG_NAME, type=float, nargs='+', required=False,
+    default=[-1.], help=MAX_PERCENTILES_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
+    help=OUTPUT_DIR_HELP_STRING
+)
+
+
+def _plot_one_field(
+        data_matrix, field_name, latitude_matrix_deg_n, longitude_matrix_deg_e,
+        border_latitudes_deg_n, border_longitudes_deg_e,
+        colour_map_object, colour_norm_object,
+        title_string, output_file_name):
+    """Plots one field.
+
+    M = number of rows in grid
+    N = number of columns in grid
+    P = number of points in border file
+
+    :param data_matrix: M-by-N numpy array of data values.
+    :param field_name: Field name (must be accepted by
+        `nwp_model_utils.check_field_name`).
+    :param latitude_matrix_deg_n: M-by-N numpy array of latitudes (deg north).
+    :param longitude_matrix_deg_e: M-by-N numpy array of longitudes (deg east).
+    :param border_latitudes_deg_n: length-P numpy array of latitudes (deg
+        north).
+    :param border_longitudes_deg_e: length-P numpy array of longitudes (deg
+        east).
+    :param colour_map_object: Colour scheme (instance of
+        `matplotlib.colors.ListedColormap` or similar).
+    :param colour_norm_object: Colour-normalizer, used to map from physical
+        values to colours (instance of `matplotlib.colors.BoundaryNorm` or
+        similar).
+    :param title_string: Title.
+    :param output_file_name: Path to output file.
+    """
+
+    figure_object, axes_object = pyplot.subplots(
+        1, 1, figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+    )
+
+    nwp_plotting.plot_field(
+        data_matrix=data_matrix,
+        field_name=field_name,
+        latitude_matrix_deg_n=latitude_matrix_deg_n,
+        longitude_matrix_deg_e=longitude_matrix_deg_e,
+        colour_map_object=colour_map_object,
+        colour_norm_object=colour_norm_object,
+        axes_object=axes_object,
+        plot_colour_bar=True
+    )
+
+    plotting_utils.plot_borders(
+        border_latitudes_deg_n=border_latitudes_deg_n,
+        border_longitudes_deg_e=border_longitudes_deg_e,
+        axes_object=axes_object,
+        line_colour=numpy.full(3, 0.)
+    )
+    plotting_utils.plot_grid_lines(
+        plot_latitudes_deg_n=numpy.ravel(latitude_matrix_deg_n),
+        plot_longitudes_deg_e=numpy.ravel(longitude_matrix_deg_e),
+        axes_object=axes_object,
+        meridian_spacing_deg=20.,
+        parallel_spacing_deg=10.
+    )
+
+    axes_object.set_xlim(
+        numpy.min(longitude_matrix_deg_e),
+        numpy.max(longitude_matrix_deg_e)
+    )
+    axes_object.set_ylim(
+        numpy.min(latitude_matrix_deg_n),
+        numpy.max(latitude_matrix_deg_n)
+    )
+    axes_object.set_title(title_string)
+
+    print('Saving figure to: "{0:s}"...'.format(output_file_name))
+    figure_object.savefig(
+        output_file_name, dpi=FIGURE_RESOLUTION_DPI,
+        pad_inches=0, bbox_inches='tight'
+    )
+    pyplot.close(figure_object)
+
+
+def _check_colour_limit_args(
+        min_colour_values, max_colour_values,
+        min_colour_percentiles, max_colour_percentiles, num_fields):
+    """Error-checks arguments pertaining to colour limits.
+
+    :param min_colour_values: See documentation at top of this script.
+    :param max_colour_values: Same.
+    :param min_colour_percentiles: Same.
+    :param max_colour_percentiles: Same.
+    :param num_fields: Number of fields to plot.
+    :return: min_colour_values: Same as input but might be modified.
+    :return: max_colour_values: Same as input but might be modified.
+    :return: min_colour_percentiles: Same as input but might be modified.
+    :return: max_colour_percentiles: Same as input but might be modified.
+    """
+
+    if (
+            len(min_colour_values) == len(max_colour_values) == 1 and
+            max_colour_values[0] < min_colour_values[0]
+    ):
+        min_colour_values = None
+        max_colour_values = None
+
+    if (
+            len(min_colour_percentiles) == len(max_colour_percentiles) == 1 and
+            max_colour_percentiles[0] < min_colour_percentiles[0]
+    ):
+        min_colour_percentiles = None
+        max_colour_percentiles = None
+
+    assert min_colour_values is not None or min_colour_percentiles is not None
+
+    if min_colour_values is not None:
+        error_checking.assert_is_numpy_array(
+            min_colour_values,
+            exact_dimensions=numpy.array([num_fields], dtype=int)
+        )
+        error_checking.assert_is_greater_numpy_array(
+            max_colour_values - min_colour_values, 0.
+        )
+
+    if min_colour_percentiles is not None:
+        error_checking.assert_is_numpy_array(
+            min_colour_percentiles,
+            exact_dimensions=numpy.array([num_fields], dtype=int)
+        )
+        error_checking.assert_is_geq_numpy_array(min_colour_percentiles, 0.)
+        error_checking.assert_is_leq_numpy_array(max_colour_percentiles, 100.)
+        error_checking.assert_is_greater_numpy_array(
+            max_colour_percentiles - min_colour_percentiles, 0.
+        )
+
+    return (
+        min_colour_values, max_colour_values,
+        min_colour_percentiles, max_colour_percentiles
+    )
+
+
+def _run(nwp_directory_name, plot_interp_data, model_name, init_time_string,
+         forecast_hour, field_names, min_colour_values, max_colour_values,
+         min_colour_percentiles, max_colour_percentiles, output_dir_name):
+    """Plots NWP data.
+
+    This is effectively the main method.
+
+    :param nwp_directory_name: See documentation at top of this script.
+    :param plot_interp_data: Same.
+    :param model_name: Same.
+    :param init_time_string: Same.
+    :param forecast_hour: Same.
+    :param field_names: Same.
+    :param min_colour_values: Same.
+    :param max_colour_values: Same.
+    :param min_colour_percentiles: Same.
+    :param max_colour_percentiles: Same.
+    :param output_dir_name: Same.
+    """
+
+    # Check input args.
+    nwp_model_utils.check_model_name(model_name)
+    init_time_unix_sec = time_conversion.string_to_unix_sec(
+        init_time_string, TIME_FORMAT
+    )
+    for f in field_names:
+        nwp_model_utils.check_field_name(f)
+
+    (
+        min_colour_values,
+        max_colour_values,
+        min_colour_percentiles,
+        max_colour_percentiles
+    ) = _check_colour_limit_args(
+        min_colour_values=min_colour_values,
+        max_colour_values=max_colour_values,
+        min_colour_percentiles=min_colour_percentiles,
+        max_colour_percentiles=max_colour_percentiles,
+        num_fields=len(field_names)
+    )
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        directory_name=output_dir_name
+    )
+
+    # Do actual stuff.
+    border_latitudes_deg_n, border_longitudes_deg_e = border_io.read_file()
+
+    if plot_interp_data:
+        nwp_file_name = interp_nwp_model_io.find_file(
+            directory_name=nwp_directory_name,
+            init_time_unix_sec=init_time_unix_sec,
+            forecast_hour=forecast_hour,
+            model_name=model_name,
+            raise_error_if_missing=True
+        )
+
+        print('Reading data from: "{0:s}"...'.format(nwp_file_name))
+        nwp_forecast_table_xarray = interp_nwp_model_io.read_file(nwp_file_name)
+    else:
+        nwp_file_name = nwp_model_io.find_file(
+            directory_name=nwp_directory_name,
+            init_time_unix_sec=init_time_unix_sec,
+            model_name=model_name,
+            allow_tar=False,
+            raise_error_if_missing=True
+        )
+
+        print('Reading data from: "{0:s}"...'.format(nwp_file_name))
+        nwp_forecast_table_xarray = nwp_model_io.read_file(nwp_file_name)
+
+    nwp_forecast_table_xarray = nwp_model_utils.subset_by_forecast_hour(
+        nwp_forecast_table_xarray=nwp_forecast_table_xarray,
+        desired_forecast_hours=numpy.array([forecast_hour], dtype=int)
+    )
+    nwpft = nwp_forecast_table_xarray
+
+    valid_time_unix_sec = (
+        init_time_unix_sec + forecast_hour * HOURS_TO_SECONDS
+    )
+    valid_time_string = time_conversion.unix_sec_to_string(
+        valid_time_unix_sec, TIME_FORMAT
+    )
+    model_name_fancy = nwp_plotting.MODEL_NAME_TO_FANCY[model_name]
+
+    for j in range(len(field_names)):
+        this_field_name_fancy = nwp_plotting.FIELD_NAME_TO_FANCY[field_names[j]]
+
+        data_matrix = nwp_model_utils.get_field(
+            nwp_forecast_table_xarray=nwpft,
+            field_name=field_names[j]
+        )[0, ...]
+
+        title_string = '{0:s} {1:s}\nInit {2:s}, valid {3:s}'.format(
+            model_name_fancy,
+            this_field_name_fancy,
+            init_time_string,
+            valid_time_string
+        )
+
+        output_file_name = (
+            '{0:s}/init={1:s}_valid={2:s}_{3:s}_{4:s}.jpg'
+        ).format(
+            output_dir_name,
+            init_time_string,
+            valid_time_string,
+            field_names[j].replace('_', '-'),
+            model_name.replace('_', '-')
+        )
+
+        if min_colour_values is None:
+            (
+                colour_map_object, colour_norm_object
+            ) = nwp_plotting.field_to_colour_scheme(
+                field_name=field_names[j],
+                min_value=
+                numpy.nanpercentile(data_matrix, min_colour_percentiles[j]),
+                max_value=
+                numpy.nanpercentile(data_matrix, max_colour_percentiles[j]),
+            )
+        else:
+            (
+                colour_map_object, colour_norm_object
+            ) = nwp_plotting.field_to_colour_scheme(
+                field_name=field_names[j],
+                min_value=min_colour_values[j],
+                max_value=max_colour_values[j]
+            )
+
+        _plot_one_field(
+            data_matrix=data_matrix,
+            field_name=field_names[j],
+            latitude_matrix_deg_n=nwpft[nwp_model_utils.LATITUDE_KEY].values,
+            longitude_matrix_deg_e=nwpft[nwp_model_utils.LONGITUDE_KEY].values,
+            border_latitudes_deg_n=border_latitudes_deg_n,
+            border_longitudes_deg_e=border_longitudes_deg_e,
+            colour_map_object=colour_map_object,
+            colour_norm_object=colour_norm_object,
+            title_string=title_string,
+            output_file_name=output_file_name
+        )
+
+
+if __name__ == '__main__':
+    INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
+
+    _run(
+        nwp_directory_name=getattr(INPUT_ARG_OBJECT, INPUT_DIR_ARG_NAME),
+        plot_interp_data=bool(
+            getattr(INPUT_ARG_OBJECT, PLOT_INTERP_DATA_ARG_NAME)
+        ),
+        model_name=getattr(INPUT_ARG_OBJECT, MODEL_ARG_NAME),
+        init_time_string=getattr(INPUT_ARG_OBJECT, INIT_TIME_ARG_NAME),
+        forecast_hour=getattr(INPUT_ARG_OBJECT, FORECAST_HOUR_ARG_NAME),
+        field_names=getattr(INPUT_ARG_OBJECT, FIELDS_ARG_NAME),
+        min_colour_values=numpy.array(
+            getattr(INPUT_ARG_OBJECT, MIN_VALUES_ARG_NAME), dtype=float
+        ),
+        max_colour_values=numpy.array(
+            getattr(INPUT_ARG_OBJECT, MAX_VALUES_ARG_NAME), dtype=float
+        ),
+        min_colour_percentiles=numpy.array(
+            getattr(INPUT_ARG_OBJECT, MIN_PERCENTILES_ARG_NAME), dtype=float
+        ),
+        max_colour_percentiles=numpy.array(
+            getattr(INPUT_ARG_OBJECT, MAX_PERCENTILES_ARG_NAME), dtype=float
+        ),
+        output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
+    )
