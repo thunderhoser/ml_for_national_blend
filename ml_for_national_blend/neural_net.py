c730982b07a4e9203e1c446f85d08fadcb20c04a
thunderhoser <lagerqui@ualberta.ca>
2024-07-03 18:44:34 -0600

    FOO

--- ml_for_national_blend/neural_net.py
+++ ml_for_national_blend/neural_net.py
@@ -2666,42 +2666,42 @@ def data_generator_fast_patches(option_dict, patch_overlap_size_2pt5km_pixels):
                 )
                 continue
 
-            # full_baseline_matrix = _read_residual_baseline_one_example(
-            #     init_time_unix_sec=init_times_unix_sec[init_time_index],
-            #     nwp_model_name=resid_baseline_model_name,
-            #     nwp_lead_time_hours=resid_baseline_lead_time_hours,
-            #     nwp_directory_name=resid_baseline_model_dir_name,
-            #     target_field_names=target_field_names,
-            #     patch_location_dict=None,
-            #     predict_dewpoint_depression=predict_dewpoint_depression,
-            #     predict_gust_factor=predict_gust_factor
-            # )
+            full_baseline_matrix = _read_residual_baseline_one_example(
+                init_time_unix_sec=init_times_unix_sec[init_time_index],
+                nwp_model_name=resid_baseline_model_name,
+                nwp_lead_time_hours=resid_baseline_lead_time_hours,
+                nwp_directory_name=resid_baseline_model_dir_name,
+                target_field_names=target_field_names,
+                patch_location_dict=None,
+                predict_dewpoint_depression=predict_dewpoint_depression,
+                predict_gust_factor=predict_gust_factor
+            )
 
-            try:
-                if do_residual_prediction and full_baseline_matrix is None:
-                    full_baseline_matrix = _read_residual_baseline_one_example(
-                        init_time_unix_sec=init_times_unix_sec[init_time_index],
-                        nwp_model_name=resid_baseline_model_name,
-                        nwp_lead_time_hours=resid_baseline_lead_time_hours,
-                        nwp_directory_name=resid_baseline_model_dir_name,
-                        target_field_names=target_field_names,
-                        patch_location_dict=None,
-                        predict_dewpoint_depression=predict_dewpoint_depression,
-                        predict_gust_factor=predict_gust_factor
-                    )
-            except:
-                warning_string = (
-                    'POTENTIAL ERROR: Could not read residual baseline for '
-                    'init time {0:s}.  Something went wrong in '
-                    '`_read_residual_baseline_one_example`.'
-                ).format(
-                    time_conversion.unix_sec_to_string(
-                        init_times_unix_sec[init_time_index], '%Y-%m-%d-%H'
-                    )
-                )
-
-                warnings.warn(warning_string)
-                full_baseline_matrix = None
+            # try:
+            #     if do_residual_prediction and full_baseline_matrix is None:
+            #         full_baseline_matrix = _read_residual_baseline_one_example(
+            #             init_time_unix_sec=init_times_unix_sec[init_time_index],
+            #             nwp_model_name=resid_baseline_model_name,
+            #             nwp_lead_time_hours=resid_baseline_lead_time_hours,
+            #             nwp_directory_name=resid_baseline_model_dir_name,
+            #             target_field_names=target_field_names,
+            #             patch_location_dict=None,
+            #             predict_dewpoint_depression=predict_dewpoint_depression,
+            #             predict_gust_factor=predict_gust_factor
+            #         )
+            # except:
+            #     warning_string = (
+            #         'POTENTIAL ERROR: Could not read residual baseline for '
+            #         'init time {0:s}.  Something went wrong in '
+            #         '`_read_residual_baseline_one_example`.'
+            #     ).format(
+            #         time_conversion.unix_sec_to_string(
+            #             init_times_unix_sec[init_time_index], '%Y-%m-%d-%H'
+            #         )
+            #     )
+            #
+            #     warnings.warn(warning_string)
+            #     full_baseline_matrix = None
 
             if full_baseline_matrix is None:
                 full_target_matrix = None
