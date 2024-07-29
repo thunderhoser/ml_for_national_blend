2020da65a2d87175bf41d746494cd816bd53663a
thunderhoser <lagerqui@ualberta.ca>
2024-07-29 12:41:00 -0600

    FOO

--- ml_for_national_blend/plot_predictions.py
+++ ml_for_national_blend/plot_predictions.py
@@ -23,6 +23,7 @@ import nbm_utils
 import urma_utils
 import misc_utils
 import neural_net
+import nwp_input
 import target_plotting
 import plotting_utils
 
@@ -394,7 +395,7 @@ def _plot_everything_1sample(
             patch_location_dict = None
 
         baseline_prediction_matrix = (
-            neural_net._read_residual_baseline_one_example(
+            nwp_input.read_residual_baseline_one_example(
                 init_time_unix_sec=init_time_unix_sec,
                 nwp_model_name=baseline_nwp_model_name,
                 nwp_lead_time_hours=lead_time_hours,
