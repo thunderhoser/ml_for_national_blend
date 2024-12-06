20b55fd57c5dc6677442d1f2ec3c7bc4b84dee86
thunderhoser <lagerqui@ualberta.ca>
2024-12-05 18:42:26 -0700

    FOO

--- ml_for_national_blend/chiu_net_pp_architecture.py
+++ ml_for_national_blend/chiu_net_pp_architecture.py
@@ -1658,7 +1658,7 @@ def create_model(option_dict):
         basic_layer_name='last_conv_simple'
     )
 
-    if not do_residual_prediction:
+    if not do_residual_prediction and output_activ_function_name is not None:
         simple_output_layer_object = architecture_utils.get_activation_layer(
             activation_function_string=output_activ_function_name,
             alpha_for_relu=output_activ_function_alpha,
@@ -1684,7 +1684,7 @@ def create_model(option_dict):
             basic_layer_name='last_conv_dd'
         )
 
-        if not do_residual_prediction:
+        if not do_residual_prediction and output_activ_function_name is not None:
             dd_output_layer_object = architecture_utils.get_activation_layer(
                 activation_function_string=
                 architecture_utils.RELU_FUNCTION_STRING,
@@ -1713,7 +1713,7 @@ def create_model(option_dict):
             basic_layer_name='last_conv_gex'
         )
 
-        if not do_residual_prediction:
+        if not do_residual_prediction and output_activ_function_name is not None:
             gf_output_layer_object = architecture_utils.get_activation_layer(
                 activation_function_string=
                 architecture_utils.RELU_FUNCTION_STRING,
